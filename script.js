f44f0f93b43e26bfdb73ce123e9e97661b8018e4
Kofi Twum-Ampofo <pktwum96@gmail.com>
2020-03-15 18:36:40 +0000

    Randomize direction of ejection

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight / 2;
 
-var colourSelected="black";
+var colourSelected = "black";
 
-document.getElementById("redSwatch").onclick = function() {colourSelected="red"};
-document.getElementById("orangeSwatch").onclick = function() {colourSelected="orange"};
-document.getElementById("yellowSwatch").onclick = function() {colourSelected="yellow"};
-document.getElementById("greenSwatch").onclick = function() {colourSelected="green"};
-document.getElementById("blueSwatch").onclick = function() {colourSelected="blue"};
-document.getElementById("indigoSwatch").onclick = function() {colourSelected="indigo"};
-document.getElementById("violetSwatch").onclick = function() {colourSelected="violet"};
-document.getElementById("blackSwatch").onclick = function() {colourSelected="black"};
+document.getElementById("redSwatch").onclick = function() {
+  colourSelected = "red"
+};
+document.getElementById("orangeSwatch").onclick = function() {
+  colourSelected = "orange"
+};
+document.getElementById("yellowSwatch").onclick = function() {
+  colourSelected = "yellow"
+};
+document.getElementById("greenSwatch").onclick = function() {
+  colourSelected = "green"
+};
+document.getElementById("blueSwatch").onclick = function() {
+  colourSelected = "blue"
+};
+document.getElementById("indigoSwatch").onclick = function() {
+  colourSelected = "indigo"
+};
+document.getElementById("violetSwatch").onclick = function() {
+  colourSelected = "violet"
+};
+document.getElementById("blackSwatch").onclick = function() {
+  colourSelected = "black"
+};
 
 
 var timerID;
@@ -22,9 +38,9 @@ var counter = 0;
 // event fires
 var position = {
   x: 0,
-  y:0
+  y: 0
 }
-var balls=[];
+var balls = [];
 var pressHoldDuration = 100;
 
 // Listening for the mouse events
@@ -36,16 +52,22 @@ function pressingDown(e) {
   requestAnimationFrame(timer);
   e.preventDefault();
 }
+
 function notPressingDown(e) {
   // Stop the timer
-  position.x=event.x;
-  position.y=Math.round(event.y-canvas.height/2 -40);
-  var ball = new Ball(position.x, position.y, counter/10, counter/10, 10, colourSelected);
-  balls.push(ball);
+  position.x = event.x;
+  position.y = Math.round(event.y - canvas.height / 2 - 40);
+  var ball = new Ball(position.x, position.y, counter / 10 * Math.floor(Math.random() * (1 - -1 + 1)) + -1, counter / 10, 10, colourSelected);
+  if (position.y <= 11 || position.x <= 11 || position.y >= canvas.height - 10 || position.x >= canvas.width - 10) {
+    console.log("too close to edge");
+  } else {
+    balls.push(ball);
+  }
   console.log(balls);
   cancelAnimationFrame(timerID);
   counter = 0;
 }
+
 function timer() {
 
   if (counter < pressHoldDuration) {
@@ -54,7 +76,8 @@ function timer() {
   }
 }
 
-
+var gravity = 0.4;
+var friction = 0.8;
 
 var context = canvas.getContext('2d');
 
@@ -76,15 +99,18 @@ function Ball(positionX, positionY, vectorX, vectorY, radius, ballColour) {
 
   this.updateAnimation = function() {
 
-    this.positionX += this.vectorX;
-    this.positionY += this.vectorY;
-    if (this.positionX + this.ballRadius > canvas.width || this.positionX - this.ballRadius < 0) {
-      this.vectorX = -this.vectorX;
+    this.positionX -= this.vectorX;
+    this.positionY -= this.vectorY;
+    if (this.positionX + this.ballRadius >= canvas.width || this.positionX - this.ballRadius <= 0) {
+      this.vectorX = -this.vectorX * friction;
     }
-
-    if (this.positionY + this.ballRadius > canvas.height || this.positionY - this.ballRadius < 0) {
-      this.vectorY = -this.vectorY;
-
+    if (this.positionY + this.ballRadius - this.vectorY >= canvas.height || this.positionY - this.ballRadius <= 0) {
+      this.vectorY = -this.vectorY * friction;
+    } else {
+      this.vectorY -= gravity;
+    }
+    if (this.positionY + this.ballRadius - this.vectorY >= canvas.height) {
+      this.vectorX = this.vectorX * friction;
     }
     this.createBall();
   }
